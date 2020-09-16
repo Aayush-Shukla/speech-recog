a3a8f1d859c2f39d15fea2c3fb4775bca5f6291b
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-09-16 22:17:36 +0530

    style changes

--- script.js
+++ script.js
@@ -11,18 +11,22 @@ recognition.lang = 'en-GB';
 recognition.interimResults = false;
 
 
+var showText=document.getElementById("command")
 
 recognition.onresult = function(event) {
+
     console.log(event)
     var last = event.results.length - 1;
     var command = event.results[last][0].transcript;
+    showText.innerHTML=`Commad Recieved: ${command}`
+
     console.log(command)
-    if(command.toLowerCase() === 'move up'){
-        pos-=step
+    if(command.toLowerCase().includes("up")){
+        posY-=step
         drawSquare()
     }
-    else if (command.toLowerCase() === 'move down'){
-        pos+=step
+    else if (command.toLowerCase().includes("down")){
+        posY+=step
         drawSquare()
     }
     }
@@ -40,6 +44,8 @@ recognition.onspeechend = function() {
 
 
 function startRecognition(){
+ 
+    showText.innerHTML=''
 recognition.start();
 
 
@@ -50,8 +56,10 @@ let ctx=canvas.getContext('2d');
 
 var size=90
 var step=40
+var posX=canvas.width/2-size/2
 
-var pos=canvas.height/2-size/2
+var posY=canvas.height/2-size/2
+
 
 // ctx.Fill();'
 
@@ -60,7 +68,8 @@ function drawSquare(){
     ctx.clearRect(0,0,canvas.width,canvas.height)
 
     ctx.beginPath();
-ctx.fillRect(canvas.width/2, pos,size, size);
+    ctx.fillStyle="red"
+ctx.fillRect(posX, posY,size, size);
 
 }
 
